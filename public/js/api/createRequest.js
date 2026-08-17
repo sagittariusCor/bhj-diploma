ebdc6e8957f5a5de7bd5aec3a87102f68d168af8
Irina <Feya_moon88@mail.ru>
2026-08-17 19:49:33 +0600

    Добавила функцию createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,53 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
+    let url = options.url;
+
+    const method = options.method || 'GET';
+    const data = options.data || {};
+    const callback = options.callback || (() => {});
+
+    if (method === 'GET') {
+        const params = new URLSearchParams(data);
+        const queryString = params.toString();
+        if (queryString) {
+            url += `?${queryString}`;
+        }
+    }
+
+    xhr.open(method, url);
+
+    if (method !== 'GET') {
+        const formData = new FormData();
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                formData.append(key, data[key]);
+            }
+        }
+
+        xhr.send(formData);
+    }else {
+        xhr.send();
+    }
+
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === xhr.DONE) {
+            let err = null;
+            let response = null;
+
+            if (xhr.status === 200) {
+                response = xhr.response;
+                if (response && response.success === false) {
+                    err = response.error || 'Произошла ошибка на сервере';
+                }
+            }else {
+                err = `Произошла ошибка при выполнении запроса. Статус: ${xhr.status} ${xhr.statusText}`;
+            }
+             callback(err, response);
+        }
+    });
+    
 };
